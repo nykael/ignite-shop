e08ca16a57f98bc2b86dde2c874dc1dad790d54e
nykael <nykaelccardial@gmail.com>
2022-11-27 12:09:23 -0300

    NEXT image and Cash APi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { globalStyles } from '../styles/global'
 import Logo from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 import Image from 'next/image'
-console.log(Logo) 
+
 
 globalStyles()
 
